60867867361cf7d3b313191ccf26c6109ca255e8
MrOmagad <99020967+MrOmagad@users.noreply.github.com>
2026-06-03 16:56:58 +0300

    вап

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,7 +3,6 @@ Console.SetBufferSize(40, 30);
 
 int x1 = 2;
 int y1 = 3;
-
 char c1 = '*';
 
 Drow(x1, y1, c1);
@@ -13,8 +12,6 @@ int y2 = 3;
 char c2 = '#';
 
 Drow(x2, y2, c2);
-
-Console.ReadLine();
 
 static void Drow(int x,  int y,  char c)
 {
