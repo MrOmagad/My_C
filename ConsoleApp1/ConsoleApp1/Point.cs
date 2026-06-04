baafc8c8ed1c815991dd334b2051b7a3e42e19c6
MrOmagad <99020967+MrOmagad@users.noreply.github.com>
2026-06-04 11:27:09 +0300

    Сделал класс Point

---
+++ ConsoleApp1/ConsoleApp1/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+  class Point
+  {
+    public int x, y;
+    public char c;
+
+     public void Draw()
+    {
+      Console.SetCursorPosition(x, y);
+      Console.WriteLine(c);
+    }
+  }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,20 +1,34 @@
-﻿Console.SetWindowSize(40, 30);
-Console.SetBufferSize(40, 30);
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-int x1 = 2;
-int y1 = 3;
-char c1 = '*';
 
-Draw(x1, y1, c1);
+namespace ConsoleApp1
+{	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(40, 30);
+			Console.SetBufferSize(40, 30);
 
-int x2 = 2;
-int y2 = 3;
-char c2 = '#';
+			int x1 = 2;
+			int y1 = 3;
+			char c1 = '*';
 
-Draw(x2, y2, c2);
+			Point p1 = new Point();
+			p1.x = 2;
+			p1.y = 3;
+			p1.c = '*';
+			p1.Draw();
 
-static void Draw(int x,  int y,  char c)
-{
-  Console.SetCursorPosition(x, y);
-  Console.WriteLine(c);
+			Point p2 = new Point();
+			p2.x = 3;
+			p2.y = 4;
+			p2.c = '#';
+			p2.Draw();
+
+			Console.ReadLine();
+		}
+	}
 }
